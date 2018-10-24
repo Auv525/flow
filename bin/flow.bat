dfb26532ff8fb4c828b8e684cf19b383411fa4a5
Auv525 <https://Auv525@github.com>
2018-10-24 14:18:49 +0800

    change flow.bat

--- bin/flow.bat
+++ bin/flow.bat
@@ -1,3 +1,2 @@
 set PYTHONPATH=D:\zhaochang\workspace\flow\pylib;%PYTHONPATH%
-REM TODO: need to change this to not use absolute paths
-D:\zhaochang\venv\Scripts\python.exe D:\zhaochang\workspace\flow\bin\flow.py %*
+C:\python27\python.exe S:\archives\code\flow\bin\flow.py %*
